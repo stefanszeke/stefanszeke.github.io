cf5259e3deb4051ed6f9716b5b34d9c26e6208f1
Stefan Szekely <sekestefan@gmail.com>
2022-09-01 13:00:03 +0200

    still trying to run svelte

--- pages/svelteTest/deploy.sh
+++ pages/svelteTest/deploy.sh
@@ -21,6 +21,6 @@ git commit -m 'deploy'
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git main
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:stefanszeke/svelte-test.git main:gh-pages
+git push -f git@github.com:stefanszeke/pages/svelteTest.git main:gh-pages
 
 cd -
