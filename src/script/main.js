b14bb07fbaec865859071c4b96578681b4064413
Stefan Szekely <sekestefan@gmail.com>
2022-10-10 19:21:50 +0200

    add: auto scrolling on mobile

--- src/script/main.js
+++ src/script/main.js
@@ -7,41 +7,64 @@ let items2 = document.querySelector("#items2");
 let downarrow = document.querySelector("#downarrow");
 
 let gridbox = document.querySelectorAll(".content");
+let logos = document.querySelectorAll("#logos");
 // let light = document.querySelector("#light");
 
 silhouette1.style.top = '150px'
 silhouette2.style.top = '200x'
 
-window.addEventListener("scroll", function () {
-  let value = window.scrollY;
-  let filters1 = `saturate(1.2) brightness(1.05) blur(0px)`
-  let filters2 = `saturate(1.5) brightness(0.95) blur(7px)`
+ 
+const isNotMobile = !window.matchMedia("(max-width: 768px)").matches
 
-  text.style.top = value * 0.7 + "px";
+// hero scroll animation
+if(isNotMobile){
+  window.addEventListener("scroll", () => {
+    let value = window.scrollY;
 
-  silhouette1.style.top = -value * 0.2 + 150 + "px";
-  silhouette2.style.top = -value * 0.15 + 200 + "px";
-  background.style.top = value * 0.5 + "px";
+    let filters1 = `saturate(1.2) brightness(1.05) blur(0px)`
+    let filters2 = `saturate(1.5) brightness(0.95) blur(7px)`
 
-  items1.style.top = -value * 0.5 + 200 + "px";
-  items2.style.top = -value * 0.6 + 330 + "px";
+    text.style.top = value * 0.7 + "px";
 
-  downarrow.style.top = value * 0.5+70 + "%";
+    silhouette1.style.top = -value * 0.2 + 150 + "px";
+    silhouette2.style.top = -value * 0.15 + 200 + "px";
+    background.style.top = value * 0.5 + "px";
+
+    items1.style.top = -value * 0.5 + 200 + "px";
+    items2.style.top = -value * 0.6 + 330 + "px";
+
+    downarrow.style.top = value * 0.5+70 + "%";
 
 
-  if(value < 400) {
-    items1.style.filter = "blur(7px)";
-    items2.style.filter = "blur(5px)";
-    background.style.filter = filters1;
-  } else {
-    items1.style.filter = "blur(0px)";
-    items2.style.filter = "blur(2px)";
-    background.style.filter = filters2;
-  }
+    if(value < 400) {
+      items1.style.filter = "blur(7px)";
+      items2.style.filter = "blur(5px)";
+      background.style.filter = filters1;
+    } else {
+      items1.style.filter = "blur(0px)";
+      items2.style.filter = "blur(2px)";
+      background.style.filter = filters2;
+    }
 
-})
+  })
+  // hero scroll for mobile
+} else {
+  window.addEventListener("scroll", (e) => {
+    if(!isScrollingUp()) {
+      if(window.pageYOffset > 0 && window.pageYOffset < 850) {
+        window.scrollTo(0, 850)
+      }
+    } else {
+      if(window.pageYOffset > 830 && window.pageYOffset < 840) {
+        window.scrollTo(0, 0)
+      }
+    }
+  })
+}
 
-gridbox.forEach(function (box) {
+
+// hover effect for mobile grid items
+gridbox.forEach((box) => {
 
   window.addEventListener("scroll", (e) => {
 
@@ -58,4 +81,16 @@ gridbox.forEach(function (box) {
 
 })
 
- 
+// helper functions
+let lastPosition = 0;
+function isScrollingUp() {
+  let isScrollingUp = false;
+  
+    let position = window.pageYOffset;
+    if(position < lastPosition) {
+      isScrollingUp = true;
+    } 
+    lastPosition = position;
+    
+  return isScrollingUp;
+}
